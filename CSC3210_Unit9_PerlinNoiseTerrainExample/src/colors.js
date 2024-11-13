a9594a38bb45554616600499551d7b1ea0a2b96d
ryancl26 <143441008+ryancl26@users.noreply.github.com>
2024-11-13 13:03:04 -0500

    experimenting with changing colors

--- CSC3210_Unit9_PerlinNoiseTerrainExample/src/colors.js
+++ CSC3210_Unit9_PerlinNoiseTerrainExample/src/colors.js
@@ -3,15 +3,15 @@ const Palette = {
     // Pink: 0xe64386,
     // Beige: 0xf6d7bd,
     // Teal: 0x035e79,
-    Peach: 0xf19c98,
-    Plum: 0x3c3951,
-    White: 0xffffff
+    LightBrown: 0xC4A484,
+    LightGreen: 0x90EE90,
+    DarkGreen: 0x013220
 };
 
 const Colors = {
-    BackgroundColor: Palette.Peach,
-    LightColor: Palette.White,
-    TerrainColor: Palette.Plum
+    BackgroundColor: Palette.LightBrown,
+    LightColor: Palette.LightGreen,
+    TerrainColor: Palette.DarkGreen
 };
 
 exports.Colors = Colors;
